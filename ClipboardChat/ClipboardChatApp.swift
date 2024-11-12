5569b3b3b8c77c340dcf7f16204f3730f4dc60fb
itsuki <itsuki.enjyoy@gmail.com>
2024-11-12 16:07:36 +0900

    Initial Commit

---
+++ ClipboardChat/ClipboardChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardChatApp.swift
+//  ClipboardChat
+//
+//  Created by Itsuki on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
